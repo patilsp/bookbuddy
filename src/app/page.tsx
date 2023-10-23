fd5935f0a07206dbbf04019edab4e553e5d811fc
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-10-23 22:57:18 +0530

    ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,27 @@ import { Menu, X } from 'lucide-react'
 import { useState } from 'react'
 
 const navigation = [
+  { name: 'Dashboard', href: '#' },
   { name: 'Fantasy', href: '#' },
-  { name: 'Mystery', href: '#' },
-  { name: 'Returns', href: '#' },
-  { name: 'About us', href: '#' },
+  { name: 'Novels', href: '#' },
+  { name: 'Educational', href: '#' },
+  { name: 'Adult 18+', href: '#' },
 ]
 
 export default function Home() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false)
   return (
     <div className='bg-white'>
-      <header className='absolute inset-x-0 top-0 z-50'>
+      <header className='bg-header sticky top-0 z-50 w-full border-b bg-header/95 backdrop-blur supports-[backdrop-filter]:bg-header/60'>
         <nav
-          className='flex items-center justify-between p-6 lg:px-8'
+          className='flex items-center justify-between p-2 lg:px-8'
           aria-label='Global'>
           <div className='flex lg:flex-1'>
-            <a href='#' className='-m-1.5 p-1.5'>
-              <span className='sr-only'>Your Company</span>
+            <a href='/' className='-m-1.5 p-1.5'>
+              <span className='sr-only'>BookBuddyPro</span>
               <img
-                className='h-8 w-auto'
-                src='/book.png'
+                className='h-12 w-auto'
+                src='/images/logo3.png'
                 alt=''
               />
             </a>
@@ -104,19 +105,9 @@ export default function Home() {
         </Dialog>
       </header>
 
-      <div className='relative isolate pt-14'>
-        <div
-          className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
-          aria-hidden='true'>
-          <div
-            className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          />
-        </div>
-        <div className='py-24 sm:py-32 lg:pb-40'>
+      <div className='relative isolate pt-14 bg-header'>
+        
+        <div className='py-12 sm:py-32 lg:pb-40'>
           <div className='mx-auto max-w-7xl px-6 lg:px-8'>
             <div className='mx-auto max-w-2xl text-center'>
               <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
